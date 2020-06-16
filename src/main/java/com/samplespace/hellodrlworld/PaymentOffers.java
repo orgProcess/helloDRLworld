9d3d6f8ced8d35c8bdc88bace5363ac6bb041351
pamAdmin <>
2020-06-16 15:41:17 -0500

    {/src/main/java/com/samplespace/hellodrlworld/PaymentOffers.java}

--- src/main/java/com/samplespace/hellodrlworld/PaymentOffers.java
+++ src/main/java/com/samplespace/hellodrlworld/PaymentOffers.java
@@ -8,6 +8,23 @@ public class PaymentOffers implements java.io.Serializable {
 
     static final long serialVersionUID = 1L;
 
+	private String channel;
+	private int discount;
+	
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
     public PaymentOffers() {
     }
 
