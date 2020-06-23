b9953119292c6ac129a8612d42e97205e548442d
pamAdmin <>
2020-06-22 23:57:14 -0500

    {/src/main/java/com/samplespace/hellodrlworld/testCustomer.java}

--- src/main/java/com/samplespace/hellodrlworld/testCustomer.java
+++ src/main/java/com/samplespace/hellodrlworld/testCustomer.java
@@ -1,13 +1,13 @@
 package com.samplespace.hellodrlworld;
 
-
-
 public class testCustomer implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
 	private String cust;
 
+	@org.kie.api.definition.type.Label(value = "Customer")
+	private com.samplespace.hellopojoworld01.Customer01 customer;
 
 	public testCustomer() {
 	}
@@ -20,9 +20,18 @@ public class testCustomer implements java.io.Serializable {
 		this.cust = cust;
 	}
 
+	public com.samplespace.hellopojoworld01.Customer01 getCustomer() {
+		return this.customer;
+	}
 
-	public testCustomer(java.lang.String cust) {
+	public void setCustomer(com.samplespace.hellopojoworld01.Customer01 customer) {
+		this.customer = customer;
+	}
+
+	public testCustomer(java.lang.String cust,
+			com.samplespace.hellopojoworld01.Customer01 customer) {
 		this.cust = cust;
+		this.customer = customer;
 	}
 
 }
